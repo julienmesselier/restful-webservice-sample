a50ca8cce55608513ffd3b968a4a60b5152acc4f
julien <julien.messelier@sgcib.com>
2017-11-12 01:15:35 +0100

    use of flask_restful

--- app.py
+++ app.py
@@ -1,34 +1,75 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import reqparse, abort, Resource, Api
+from http import HTTPStatus
 
 app = Flask(__name__)
+api = Api(app)
 
-
-books = [
-    {
-        'id': 1,
+books = {
+    '1': {
         'title': 'Domain-Driven Design: Tackling Complexity in the Heart of Software',
         'author': 'Eric Evans',
         'cover': 'Hardcover'
     },
-    {
-        'id': 2,
-        'title': 'Gödel, Escher, Bach: An Eternal Golden Braid',
+    '2': {
+        'title': 'Goedel, Escher, Bach: An Eternal Golden Braid',
         'author': 'Douglas Hofstadter',
         'cover': 'Paperback'
     },
-    {
-        'id': 3,
+    '3': {
         'title': 'Thinking in Systems',
         'author': 'Donella H. Meadows',
         'cover': 'Paperback'
     }
-]
+}
 
 
-@app.route('/library/api/v1.0/books')
-def get_books():
-    return jsonify({'books': books})
+def abort_if_book_does_not_exist(book_id):
+    if book_id not in books:
+        abort(HTTPStatus.NOT_FOUND, message="Book {} does not exit".format(book_id))
 
+
+parser = reqparse.RequestParser()
+parser.add_argument('title', type=str, help='title of the book')
+parser.add_argument('author', type=str, help='author of the book')
+parser.add_argument('cover', type=str, help='cover type: Paperback / Hardcover')
+
+
+class Book(Resource):
+    def get(self, book_id):
+        abort_if_book_does_not_exist(book_id)
+        return {book_id: books[book_id]}
+
+    def delete(self, book_id):
+        abort_if_book_does_not_exist(book_id)
+        del books[book_id]
+        return '', HTTPStatus.NO_CONTENT
+
+    def put(self, book_id):
+        args = parser.parse_args()
+        book = {'title': args['title'],
+                'author': args['author'],
+                'cover': args['cover']}
+        books[book_id] = book
+        return book, HTTPStatus.CREATED
+
+
+class BookList(Resource):
+    def get(self):
+        return books
+
+    def post(self):
+        args = parser.parse_args()
+        book = {'title': args['title'],
+                'author': args['author'],
+                'cover': args['cover']}
+        todo_id = str(int(max(books.keys())) + 1)
+        books[todo_id] = book
+        return book, HTTPStatus.CREATED
+
+
+api.add_resource(Book, '/library/api/v1.0/book/<string:book_id>')
+api.add_resource(BookList, '/library/api/v1.0/books')
 
 if __name__ == '__main__':
     app.run(debug=True)
